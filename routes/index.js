99caabe58d2a0e1c5fb9495bb56dfaa4233981b8
사과 <sbd2953@gmail.com>
2025-11-14 10:58:24 +0900

    템플릿엔지이해

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 
 var express = require('express');
 var router = express.Router();
-
 /* GET home page. 
 localhost:3000 요청하면 7번 인터셉트 해서 home.ejs요청한다.
 */
@@ -9,6 +8,14 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Home', pageName: 'pages/home.ejs' });
 });
 // 회원가입 페이지 추가
+// index.js에 설정한 endpoint에는 users를 사용하지 않습니다.
+// 위 설정은 app.js에서 확인할 수 있다. -> routing설정이 여기
+/*
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+*/
 // http://localhost:3000/signup
 // http://127.0.0.1:3000/signup
 // http://192.168.0.41:3000/signup
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
-var express = require('express');
-var router = express.Router();
-
+// 의존성 주입으로 설치한 express프레임워크 객체를 참조한다.
+// npm i express
+// npm i -D express
+const express = require('express');//모듈(CJS, ESM)
+router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
@@ -9,7 +11,8 @@ router.get('/', function(req, res, next) {
 //get메서드의 두번째 파라미터에는 요청객체, 응답객체, 미들웨어 연결
 //요청객체는 주로 사용자가 입력한 값을 서버에서 요청할 때
 //응답객체는 주로 mimetype설정하기, 응답페이지 요청하기,,,
-//마이페이지
+//마이페이지 - 인증 후에 - 로그인을 한 다음에 보여지는 메뉴입니다.
+//-> http://localhost:3000/users/mypage
 router.get('/mypage', function(req, res, next) {
   //응답객체는 send메서드와 render메서드를 제공함
   //send는 파라미터에 있는 문자열이 출력되고 우리는 mypage화면을 출력할거니까 render함수를 호출함
@@ -17,6 +20,7 @@ router.get('/mypage', function(req, res, next) {
   res.render('index', {title: '마이페이지', pageName: 'pages/users/mypage.ejs'})
 });
 //회원정보수정
+// -> http://localhost:3000/users/memberUpdate -> 404번
 router.get('/memberUpdate', function(req, res, next) {
   res.render('index', {title: '회원정보수정', pageName: 'pages/users/memberUpdate.ejs'})
 });
@@ -25,5 +29,15 @@ router.get('/memberUpdate', function(req, res, next) {
 router.get('users/cart', function(req, res, next) {
   res.render('index', {title: '장바구니', pageName: 'pages/users/cart.ejs'})
 });
+//회원가입
+//-> http://localhost:3000/users/join
+router.get('/join', function(req, res, next) {
+  res.render('index', {title: '회원가입', pageName: 'pages/users/join.ejs'})
+});
+router.post('/join2', function(req, res, next) {
+  //응답을 render함수 사용하면 mime type -> text/html이고
+  res.send('<h1>post</h1>요청 테스트');//mime type ->text/plain
+});
 module.exports = router;
+
 
